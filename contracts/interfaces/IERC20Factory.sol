dfe5c1b847cbb52e77b11bd171ff1e98d253552b
Dalos Network <125082532+dalosnetwork@users.noreply.github.com>
2025-05-05 19:41:22 +0300

    Delete contracts directory

--- contracts/interfaces/IERC20Factory.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-interface IERC20Factory {
-    function createERC20(string memory name, string memory symbol, uint256 totalSupply, address _owner) external returns (address);
-}
--- contracts/interfaces/IStorage.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-interface IStorage {
-    function getTokenNFTPair(address _tokenAddress) external view returns (uint256);
-    function getNftTokenPair(uint256 _tokenID) external view returns (address);
-    function getCleanList(address _userAddress) external view returns (bool);
-    function setPair(address _tokenAddress, uint256 _tokenID) external;
-}
