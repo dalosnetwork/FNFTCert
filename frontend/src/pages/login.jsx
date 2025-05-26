4208eb989edc20bcfeab21a35d76f7b4b308031a
Ali Nazım Küçük <artnevka@Ali-MacBook-Air.local>
2025-05-26 19:38:50 +0300

    css

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,39 +1,53 @@
 import { useState } from "react";
-import logo from "../design/assets/logo-siyah.svg"
+import logo from "../design/assets/logo-siyah.svg";
 import Button1 from "../components/button1";
 import Icon from "../components/iconManager";
 
 function Login() {
-  const [count, setCount] = useState(0);
-
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorState, setErrorState] = useState(false);
   return (
     <section id="login">
       <div className="container">
         <div className="row">
-          <div className="col-12" style={{padding:"60px 0"}}>
-            <img src={logo} style={{width:"140px"}} alt="" />
+          <div className="col-12" style={{ padding: "60px 0" }}>
+            <img src={logo} style={{ width: "140px" }} alt="" />
           </div>
           <div className="col-12 d-flex justify-content-center">
-            <div className="loginWrapper">
-              <div className="row d-flex" style={{gap:"10px"}}>
-                <div className="col-12 font24 bold">
-                  Login
-                </div>
+            <div className={`loginWrapper ${errorState ? `error` : ``}`}>
+              <div className="row d-flex" style={{ gap: "10px" }}>
+                <div className="col-12 font24 bold">Login</div>
                 <div className="col-12 font16 semibold">
                   Email
-                  <div className="loginInput">
+                  <div className="loginInput mt-2">
                     <input type="text" placeholder="Email" />
                   </div>
                 </div>
                 <div className="col-12 font16 semibold">
                   Password
-                  <div className="loginInput">
-                    <input className="password" type="text" placeholder="Password" />
-                    <Icon name="passwordHide"/>
+                  <div className="loginInput mt-2">
+                    <input
+                      className="password"
+                      type={showPassword ? `text` : `password`}
+                      placeholder="Password"
+                    />
+                    <span
+                      className="pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      <Icon
+                        name={showPassword ? `passwordShow` : `passwordHide`}
+                      />
+                    </span>
                   </div>
                 </div>
+                {errorState && (
+                  <div className="col-12 semibold errorMsg font14">
+                    Wrong email or password, please try again.
+                  </div>
+                )}
                 <div className="col-12 d-flex justify-content-center">
-                  <Button1 className={`login disabled`} label={"Login"}/>
+                  <Button1 onClick={()=>setErrorState(!errorState)} className={`login disabled`} label={"Login"} />
                 </div>
               </div>
             </div>
